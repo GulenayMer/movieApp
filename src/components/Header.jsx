eac8fb0624c27e187457d6b089e85c4f6adf1d1f
GulenayMer <mervegulenay@gmail.com>
2023-07-31 22:57:57 +0200

    toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,89 @@
 import {NavLink} from 'react-router-dom'
+import { useState, useRef, useEffect } from 'react';
 import Intro from "./Intro"
 
+const toggleBtn = `
+inline-flex items-center p-2 w-10 h-10 
+justify-center 
+text-sm text-gray-500 rounded-lg md:hidden
+hover:bg-gray-100
+focus:outline-none focus:ring-2 
+focus:ring-gray-200`
+
 const Header = () => {
+
+	const [hamburgerMenu, setHamburgerMenu] = useState(false);
+	const menuRef = useRef(null);
+
+	const handleHamburgerMenu = () => {
+		setHamburgerMenu(!hamburgerMenu);
+	}
+
+	const handleOutsideClick = (e) => {
+		if (hamburgerMenu && menuRef.current && !menuRef.current.contains(e.target)) {
+			setHamburgerMenu(false);
+		}
+	};
+
+	useEffect( () => {
+		document.addEventListener('click', handleOutsideClick);
+    // Clean up event listener when the component unmounts
+    return () => {
+    	document.removeEventListener('click', handleOutsideClick);
+    };
+	}, [])
+
 return (
-	<div>
-		<nav className='bg-slate-900 p-3 flex-col rounded-sm flex md:flex-row justify-between'>
-			<span className='text-gray-200'>WatchMovies</span>
-			<ul className="flex justify-center items-center md:flex-row ">
-				{[
-					['Home', '/'],
-					['Movies', '/movies'],
-					['Add Movie', 'movies/new'],
-					['Contact', '/contact']
-				].map(([title, url]) => (
-					<li key={url} className="font-medium  px-3 py-2 text-slate-200 rounded-md
-					hover:bg-slate-300 hover:text-slate-900">
-						<NavLink to={url}>{title}</NavLink>
-					</li>	
-				))}
+	<>
+	<nav className="border-gray-200 bg-gray-100">
+	<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 relative">
+		<a href="#" className="flex items-center">
+			<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MovieTime</span>
+		</a>
+		<button type="button" aria-expanded={hamburgerMenu}  onClick={handleHamburgerMenu} className={toggleBtn}>
+			<svg className="w-5 h-5 relative" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+					<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+			</svg>
+		</button>
+		
+		{ !hamburgerMenu ? (
+		<div ref={menuRef} className="hidden w-full md:block md:w-auto">
+			<ul className="flex flex-col font-medium mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
+			{[
+				['Home', '/'],
+				['Movies', '/movies'],
+				['Add Movie', 'movies/new'],
+				['Contact', '/contact']
+			].map(([title, url]) => (
+				<li key={url} >
+					<a href="#" className="block hover:bg-gray-200 text-gray-700 md:p-1" 
+					aria-current="page"><NavLink to={url}>{title}</NavLink></a>
+				</li>	
+			))}
 			</ul>
-		</nav>
-		<Intro></Intro>
+		</div>
+		) : (
+			<div ref={menuRef} className="absolute z-50 top-16 right-0 px-4 w-60 text-center list-none divide-y divide-gray-100 rounded-lg shadow">
+			<ul className="flex flex-col font-medium  rounded-lg bg-gray-50">
+			{[
+				['Home', '/'],
+				['Movies', '/movies'],
+				['Add Movie', 'movies/new'],
+				['Contact', '/contact']
+			].map(([title, url]) => (
+				<li key={url} >
+					<a href="#" className="block py-3 pl-3 pr-4  ounded text-red-700 md:p-0" 
+					aria-current="page"><NavLink to={url}>{title}</NavLink></a>
+				</li>	
+			))}
+			</ul>
+		</div>
+		)}
 	</div>
+	</nav>
+
+	<Intro></Intro>
+	</>
 )
 }
 
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,7 +8,7 @@ return (
 	bg-purple-200 bg-blend-multiply">
     <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-			Discover the Worlds Best Movies
+			Discover the Best Movies
 		</h1>
         <p className="mb-8 text-lg font-normal text-gray-100 lg:text-xl sm:px-16 lg:px-48">
 			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias consequuntur dolorem vitae similique aliquam,
